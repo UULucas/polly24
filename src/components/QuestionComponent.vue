474138c919b0e9606f48583cb9319354c6ada9d0
EdvinMartna <edvinmartna2@gmail.com>
2024-12-13 11:00:31 +0100

    added styles to QuestionComponent

--- src/components/QuestionComponent.vue
+++ src/components/QuestionComponent.vue
@@ -1,8 +1,13 @@
+@import 'src/assets/main.css'
 <template>
+  <div class="textbox">
 <p>{{question.q}}</p>
-<button v-for="a in question.a" v-on:click="answer(a)" v-bind:key="a">
+</div>
+<div class="answerbox link-wrapper">
+<button class="rectangle nav-button" v-for="a in question.a" v-on:click="answer(a)" v-bind:key="a">
   {{ a }}
 </button>
+</div>
 </template>
 <script>
 export default {
@@ -18,3 +23,26 @@ export default {
   }
 }
 </script>
+<style>
+.textbox{
+font-size: 3vw;
+padding:20px;
+background-color: var(--p-blue);
+border:5px solid black;
+display:inline-block;
+width:auto;
+height:auto;
+}
+.answerbox {
+  margin-top: 1rem;
+  display: grid;
+  grid-template-areas: "a b" "c d" "e f";
+  align-items: center;
+  gap: 1rem;
+}
+.rectangle {
+  width: 20rem;
+  height: 10rem;
+}
+
+</style>
