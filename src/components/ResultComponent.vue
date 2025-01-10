70061a891ae19cc30fa4dff50a27dc63c11182b1
Fr0dr1k <77593162+Fr0dr1k@users.noreply.github.com>
2025-01-10 15:20:30 +0100

    Update ResultComponent.vue

--- src/components/ResultComponent.vue
+++ src/components/ResultComponent.vue
@@ -74,7 +74,7 @@
 import BarsComponent from '@/components/BarsComponent.vue';
 import io from 'socket.io-client';
 
-const socket = io("localhost:3000");
+const socket = io(sessionStorage.getItem("dataServer"));
 
 export default {
   name: 'ResultComponent',
